5292daefbf2ad973c03e73a588844f38d21c63e1
Pat Rocchio <patrocc6@gmail.com>
2017-05-21 08:06:49 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT title
-  FROM series INNER JOIN characters
+  FROM series JOIN characters
   ON series.id = characters.series_id
   WHERE characters.species = 'human'
   GROUP BY title
@@ -27,12 +27,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT name, COUNT(books.id)
+  "SELECT name, COUNT(book_id)
   FROM characters
-  INNER JOIN character_books
+  JOIN character_books
   ON characters.id = character_books.character_id
-  INNER JOIN books
-  ON books.id = character_books.book_id
   GROUP BY characters.name
-  ORDER BY COUNT(books.id) DESC;"
+  ORDER BY COUNT(book_id) DESC;"
 end
